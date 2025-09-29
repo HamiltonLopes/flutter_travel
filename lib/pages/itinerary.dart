71d58eaccc22c714523caa13080d775697477a7e
Hamilton Lopes <hamilton_dino02@hotmail.com>
2025-09-28 22:23:04 -0300

    feat: apply first colors and starting drawer design

---
+++ lib/pages/itinerary.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class ItineraryPage extends StatefulWidget {
+  const ItineraryPage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<ItineraryPage> createState() => _ItineraryPageState();
+}
+
+class _ItineraryPageState extends State<ItineraryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: Text(
+          widget.title,
+          style: TextStyle(color: Theme.of(context).colorScheme.secondary),
+        ),
+        iconTheme: IconThemeData(
+          color: Theme.of(context).colorScheme.secondary, // Cor do ícone
+        ),
+      ),
+      drawer: Drawer(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Itinerary Page',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
